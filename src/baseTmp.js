d4cc43926b7a38b68af7c712f53aa6f703dd2a5e
dankoknad <dankomilutinovic@gmail.com>
2017-03-14 16:45:29 +0100

    Updates

--- src/baseTmp.js
+++
@@ -1,9 +0,0 @@
-import Rebase from 're-base';
-
-const base = Rebase.createClass({
-    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    authDomain: "xxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    databaseURL: "xxxxxxxxxxxxxxxxxxxxxxxxxx"
-});
-
-export default base;
