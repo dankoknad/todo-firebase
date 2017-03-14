dfdbd961fb0235aa51116ad101458519f699c17a
dankoknad <dankomilutinovic@gmail.com>
2017-03-14 16:35:47 +0100

    added remove and toggle functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import uuidV4 from 'uuid/v4';
+import _ from 'lodash';
 
 import base from './base';
 
@@ -32,18 +34,61 @@ class App extends Component {
 
   updateList = (e) => {
     e.preventDefault();
+    const newItem = {
+      id: uuidV4(),
+      txt: this.state.tmpItem,
+      isDone: false
+    }
 
     this.setState({
-      items: [...this.state.items, this.state.tmpItem],
+      items: [newItem, ...this.state.items],
       tmpItem: ''
     })
   }
 
+  deleteItem = (e) => {
+    e.preventDefault();
+    const removeIndex = this.state.items.findIndex(item => item.id === e.target.getAttribute('data-index'));
+    
+    this.setState({
+      items: [
+        ...this.state.items.slice(0, removeIndex),
+        ...this.state.items.slice(removeIndex+1)
+      ]
+    })
+        
+    console.log("removeIndex", removeIndex);
+  }
+
+  toggleTodo = (id) => {
+    const whichTodo = _.find(this.state.items, { 'id': id });
+    const index = _.findIndex(this.state.items, { 'id': id });
+    const edited = {...whichTodo, isDone : !whichTodo.isDone};
+    
+    this.setState({
+      items: [
+        ...this.state.items.slice(0, index),
+        edited,
+        ...this.state.items.slice(index+1)
+      ]
+    })
+
+
+  }
+
   render() {
     const list = (this.state.items.length) 
-      ? this.state.items.map((item,i) => <li key={i}> {item} </li>) 
+      ? this.state.items.map((item) => 
+        <li key={item.id}>
+          <input type="checkbox" onChange={() => this.toggleTodo(item.id)} checked={item.isDone}/> 
+          <span className={item.isDone ? "done" : null}>{item.txt}</span> 
+          <button onClick={this.deleteItem} data-index={item.id}> x </button> 
+        </li>
+      ) 
       : <li>Loading ...</li>;
-  
+    
+    // console.log("state ", this.state.items);
+
     return (
       <div className="App">
         <div className="App-header">
@@ -54,12 +99,12 @@ class App extends Component {
           Hello
         </p>
         <br/>
-        <ul>
-          { list }
-        </ul>
         <form onSubmit={this.updateList}>
           <input onChange={this.updateTempItem} value={this.state.tmpItem} />
         </form>
+        <ul>
+          { list }
+        </ul>
       </div>
     );
   }
---
+++ src/baseTmp.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    databaseURL: "xxxxxxxxxxxxxxxxxxxxxxxxxx"
+});
+
+export default base;
