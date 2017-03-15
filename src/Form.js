ee37c0bedbc48139c6ea3ab03f3ac8768b92d52f
dankoknad <dankomilutinovic@gmail.com>
2017-03-15 16:39:56 +0100

    added submit button

--- src/Form.js
+++ src/Form.js
@@ -5,11 +5,16 @@ export default class Form extends Component {
 		const {onSubmit, onChange, value} = this.props;
 		return (
 			<form className="form" onSubmit={onSubmit}>
-				<input className="form-control" 
+				<div className="input-group">
+					<input className="form-control" 
 						onChange={onChange} 
 						value={value}
 						placeholder="Next todo.."
-				/>
+					/>
+					<div className="input-group-btn">
+						<button className="btn btn-default" type="submit" value="Submit">submit</button>
+					</div>
+				</div>
 			</form>
 		)
 	}
